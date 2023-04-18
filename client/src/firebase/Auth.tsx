df7cdfc55a635f076a39364fa27c2d9d78af7303
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-04-18 14:14:49 -0400

    Firebase context

---
+++ client/src/firebase/Auth.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, PropsWithChildren } from 'react';
+import firebase from 'firebase/compat';
+import firebaseApp from './Firebase';
+
+type AuthProviderState = {
+  currentUser: firebase.User | null,
+  loadingUser: boolean
+};
+
+export const AuthContext = React.createContext<firebase.User | null>(null);
+
+export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [state, setState] = useState<AuthProviderState>({
+    currentUser: null,
+    loadingUser: true
+  });
+
+  useEffect(() => {
+    return firebaseApp.auth().onAuthStateChanged((user) => {
+      setState({
+        currentUser: user,
+        loadingUser: false
+      })
+    });
+  }, []);
+
+  if (state.loadingUser)
+    return <div className='spinner'></div>;
+
+  return (
+    <AuthContext.Provider value={state.currentUser}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
