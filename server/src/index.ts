1c11186f87ec0fccab52a2128cd181adeeea766d
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-04-22 18:44:29 -0400

    Graphql schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,21 @@ import { config } from "dotenv";
 config();
 import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { readFileSync } from "fs";
+
+interface AuthContext {
+  uid?: String;
+}
 
 const app = initializeApp({
   credential: applicationDefault()
 });
 
+const typeDefs = readFileSync('../schema.graphql', { encoding: 'utf-8' });
+
+const server = new ApolloServer<AuthContext>({
+  typeDefs,
+  resolvers
+});
