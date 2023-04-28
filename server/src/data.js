a3aad8022a629b7edbf4f7aeaa7fd669105412b9
Jacob Ashkenas <jashkena@stevens.edu>
2023-04-28 14:11:55 -0400

    Mutators

--- server/src/data.js
+++ server/src/data.js
@@ -1,5 +1,15 @@
 import { GraphQLError } from "graphql";
 import { users } from "./mongo/collections";
+import { ObjectId } from "mongodb";
+
+const notFound = (msg) => {
+  throw new GraphQLError(msg, {
+    extensions: {
+      code: 'NOTFOUND',
+      http: { status: 404 }
+    }
+  });
+};
 
 export const stringify = (document) => {
   return { ...document, _id: document._id.toString() };
@@ -23,4 +33,252 @@ export const getUser = async (uid) => {
   } else {
     return res;
   }
+};
+
+const getFromUserArray = async (array, id, name) => {
+  const col = await users();
+  const res = await col.findOne({
+    [array]: {
+      $elemMatch: {
+        _id: ObjectId(id)
+      }
+    }
+  }, {
+    project: {
+      firebaseId: 1,
+      [`${array}.$`]: 1
+    }
+  });
+
+  if (!res) notFound(`${name} not found.`);
+
+  return [res.firebaseId, res[array][0]];
+}
+
+export const getUserEquation = async (id) => {
+  return await getFromUserArray('equations', id, 'Equation');
+};
+
+export const getUserProject = async (id) => {
+  return await getFromUserArray('projects', id, 'Project');
+};
+
+const getFromProjectArray = async (array, pid, id, name) => {
+  const col = await users();
+  const res = await col.findOne({
+    projects: {
+      $elemMatch: {
+        _id: ObjectId(pid),
+        [`${array}._id`]: ObjectId(id)
+      }
+    }
+  }, {
+    project: {
+      firebaseId: 1,
+      [`projects.$.${array}`]: {
+        $elemMatch: {
+          _id: ObjectId(id)
+        }
+      }
+    }
+  });
+
+  if (!res) notFound(`${name} not found.`);
+
+  return [res.firebaseId, res.projects[0][array][0]];
+};
+
+export const getProjectEquation = async (pid, id) => {
+  return await getFromProjectArray('equations', pid, id, 'Equation');
+};
+
+export const getProjectResponse = async (pid, id) => {
+  return await getFromProjectArray('responses', pid, id, 'Response');
+};
+
+export const newProject = async (uid) => {
+  const col = await users();
+  const pid = new ObjectId();
+  const res = await col.updateOne({
+    firebaseId: uid
+  }, {
+    $push: {
+      projects: {
+        _id: pid,
+        name: 'New Project',
+        lastEdited: Date.now(),
+        equations: [],
+        responses: []
+      }
+    }
+  });
+
+  if (!res.acknowledged || !res.modifiedCount)
+    throw new GraphQLError('Failed to create new project.');
+
+  return await getUserProject(pid.toString())
+};
+
+export const updateEquation = async (uid, id, name, text) => {
+  const col = await users();
+  const res = await col.updateOne({
+    firebaseId: uid,
+    equations: {
+      $elemMatch: {
+        _id: ObjectId(id)
+      }
+    }
+  }, {
+    $set: {
+      "equations.$": {
+        name: name,
+        text: text
+      }
+    }
+  });
+
+  if (!res.acknowledged)
+    throw new GraphQLError('Failed to update equation.');
+  
+  return await getUserEquation(id);
+};
+
+const addToProjectArray = async (uid, pid, array, name, text) => {
+  const col = await users();
+  const id = new ObjectId();
+  const res = await col.updateOne({
+    firebaseId: uid,
+    projects: {
+      $elemMatch: {
+        _id: ObjectId(pid)
+      }
+    }
+  }, {
+    $push: {
+      [`projects.$.${array}`]: {
+        _id: id,
+        name: name || "New",
+        text: text || ""
+      }
+    }
+  });
+
+  const t = array.substring(0, array.length - 1);
+
+  if (!res.acknowledged)
+    throw new GraphQLError(`Failed to add new ${t}.`);
+
+  return await getFromProjectArray(array, pid, id.toString(), t[0].toUpperCase() + t.substring(1));
+};
+
+export const addProjectEquation = async (uid, pid) => {
+  return await addToProjectArray(uid, pid, 'equations', 'New Equation', 'y=x^2+5');
+};
+
+export const addProjectResponse = async (uid, pid) => {
+  return await addToProjectArray(uid, pid, 'responses', 'New Response', 'The answer is c.');
+};
+
+const removeFromProjectArray = async (uid, pid, id, array) => {
+  const col = await users();
+  const res = await col.updateOne({
+    firebaseId: uid,
+    projects: {
+      $elemMatch: {
+        _id: ObjectId(pid)
+      }
+    }
+  }, {
+    $pop: {
+      [`projects.$.${array}`]: {
+        _id: ObjectId(id)
+      }
+    }
+  });
+
+  const t = array.substring(0, array.length - 1);
+
+  if (!res.acknowledged)
+    throw new GraphQLError(`Failed to remove ${t}.`);
+
+  return await getUserProject(pid);
+};
+
+export const removeProjectEquation = async (uid, pid, id) => {
+  return await removeFromProjectArray(uid, pid, id, 'equations');
+};
+
+export const removeProjectResponse = async (uid, pid, id) => {
+  return await removeFromProjectArray(uid, pid, id, 'responses');
+};
+
+export const removeUserEquation = async (uid, id) => {
+  const col = await users();
+  const res = await col.updateOne({
+    firebaseId: uid
+  }, {
+    $pop: {
+      equations: {
+        _id: ObjectId(id)
+      }
+    }
+  });
+
+  if (!res.acknowledged)
+    throw new GraphQLError('Failed to remove user equation.');
+
+  return await getUser(uid);
+};
+
+const updateInProjectArray = async (uid, pid, id, array, name, text) => {
+  const col = await users();
+  const res = await col.updateOne({
+    firebaseId: uid,
+    projects: {
+      $elemMatch: {
+        _id: ObjectId(pid)
+      }
+    }
+  }, {
+    $set: {
+      [`projects.$.${array}.$[e]`]: {
+        name: name,
+        text: text
+      }
+    }
+  }, {
+    arrayFilters: [ { "e": { _id: ObjectId(id) } } ]
+  });
+
+  if (!res.acknowledged)
+    throw new GraphQLError('Failed to update project.');
+};
+
+export const updateProject = async (uid, id, name, equations, responses) => {
+  const col = await users();
+  if (name && (name = name.trim())) {
+    const res = await col.updateOne({
+      firebaseId: uid,
+      projects: {
+        $elemMatch: {
+          _id: ObjectId(id)
+        }
+      }
+    }, {
+      $set: { "projects.$.name": name }
+    });
+  
+    if (!res.acknowledged)
+      throw new GraphQLError('Failed to update project.');
+  }
+  
+  for (const e of (equations || [])) {
+    await updateInProjectArray(uid, id, e._id, "equations", e.name, e.text);
+  }
+  
+  for (const e of (responses || [])) {
+    await updateInProjectArray(uid, id, e._id, "responses", e.name, e.text);
+  }
+
+  return await getUserProject(id);
 };
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,9 +1,9 @@
 import { GraphQLError } from "graphql";
-import { Resolvers } from "./typings/graphql";
-import { getUser, stringify } from "./data";
+import { addProjectEquation, addProjectResponse, getUser, getUserProject, newProject, removeProjectEquation, removeProjectResponse, removeUserEquation, stringify, updateEquation } from "./data";
+import { getUserEquation } from "./data";
 
 const authorize = (user, ctx) => {
-  if (user.firebaseId === ctx.uid) return;
+  if (user === ctx.uid) return;
 
   throw new GraphQLError('Unauthorized', {
     extensions: {
@@ -24,46 +24,61 @@ const userEquations = async (_, _args, ctx) => {
 };
 
 const project = async (_, { id }, ctx) => {
-  const user = await getUser(ctx.uid);
-  const projects = user.projects.map(stringify);
-  const project = projects.find(e => e._id === id);
-  if (!project) {
-    throw new GraphQLError('Project not found.', {
-      extensions: {
-        code: 'NOTFOUND',
-        http: { status: 404 }
-      }
-    });
-  }
-  return project;
+  // const user = await getUser(ctx.uid);
+  // const projects = user.projects.map(stringify);
+  // const project = projects.find(e => e._id === id);
+  // if (!project) {
+  //   throw new GraphQLError('Project not found.', {
+  //     extensions: {
+  //       code: 'NOTFOUND',
+  //       http: { status: 404 }
+  //     }
+  //   });
+  // }
+  // return project;
+  const [user, res] = getUserProject(id);
+  authorize(user, ctx);
+  return stringify(res);
 };
 
 const equation = async (_, { id }, ctx) => {
-  const user = await getUser(ctx.uid);
-  const equations = user.equations.map(stringify);
-  const equation = equations.find(e => e._id === id);
-  if (!equation) {
-    throw new GraphQLError('Equation not found.', {
-      extensions: {
-        code: 'NOTFOUND',
-        http: { status: 404 }
-      }
-    });
-  }
-  return equation;
+  // const user = await getUser(ctx.uid);
+  // const equations = user.equations.map(stringify);
+  // const equation = equations.find(e => e._id === id);
+  // if (!equation) {
+  //   throw new GraphQLError('Equation not found.', {
+  //     extensions: {
+  //       code: 'NOTFOUND',
+  //       http: { status: 404 }
+  //     }
+  //   });
+  // }
+  // return equation;
+  const [user, res] = getUserEquation(id);
+  authorize(user, ctx);
+  return stringify(res);
 };
 
-// const response = async (_, { id }, ctx) => {
-//   return null;
-// };
+const resolve = async (prom) => {
+  return stringify(await prom);
+};
 
 const Resolvers = {
   Query: {
     userProjects: userProjects,
     userEquations: userEquations,
     project: project,
-    equation: equation,
-    // response: response
+    equation: equation
+  },
+  Mutation: {
+    newProject: (_, __, ctx) => resolve(newProject(ctx.uid)),
+    // updateProject,
+    updateEquation: (_, args, ctx) => resolve(updateEquation(ctx.uid, args.id, args.name, args.text)),
+    addProjectEquation: (_, args, ctx) => resolve(addProjectEquation(ctx.uid, args.id)),
+    addProjectResponse: (_, args, ctx) => resolve(addProjectResponse(ctx.uid, args.id)),
+    removeProjectEquation: (_, args, ctx) => resolve(removeProjectEquation(ctx.uid, args.pid, args.id)),
+    removeProjectResponse: (_, args, ctx) => resolve(removeProjectResponse(ctx.uid, args.pid, args.id)),
+    removeUserEquation: (_, args, ctx) => resolve(removeUserEquation(ctx.uid, args.id))
   }
 };
 
