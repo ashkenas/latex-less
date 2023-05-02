72d667d90a0de54096e8b9c668faa0cd7b2d4a0b
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-05-02 12:34:31 -0400

    frontend: nicer login page & github login

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,19 +1,24 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../firebase/Firebase";
 import "../styles/Login.scss";
 
-const gProvider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 
 export default function Login() {
   
   return (
-    <section className="section">
-      <div className="container">
-        <button className="google-btn" onClick={() => signInWithPopup(auth, gProvider)}>
-          <img src="/img/btn_google_signin_light_normal_web@2x.png" alt="Google sign-in"/>
-        </button>
-      </div>
-    </section>
+    <div className="center-center">
+      <h1 className="title is-1">LaTeXLess</h1>
+      <button className="social-btn" onClick={() => signInWithPopup(auth, googleProvider)}>
+        <img src="/img/google-signin.svg" alt="Sign in with Google"/>
+        Sign in with Google
+      </button>
+      <button className="social-btn" onClick={() => signInWithPopup(auth, githubProvider)}>
+        <img src="/img/github-mark.png" alt="Sign in with GitHub"/>
+        Sign in with GitHub
+      </button>
+    </div>
   );
 };
